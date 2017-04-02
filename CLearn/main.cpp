9b75e5e2250c8e1f1235881781d3c1ae82908f9b
Grbiezca <grabiezca@op.pl>
2017-04-02 11:43:46 +0200

    Michał może być?
    chyba działa, menu z dunkcją cls. Git?

--- CLearn/main.cpp
+++ CLearn/main.cpp
@@ -1,14 +1,65 @@
 #include <iostream>
-
+#include <stdlib.h>
+#include <math.h>
 using namespace std;
-int funkcja()
+
+class MenuPoczatkowe
 {
-    return 0;
-}
+private:
+    int x,y;
+public:
+    void funkcja()
+    {
+        menoglowne:
+        system( "cls" );
+        cout<<"Witaj w programie tworzonym przez Kruksu & Grabiezca"<< endl << "Wybierz czyja zawartosc chcesz przegladac: "<<endl;
+        cout<<"0-Kruksu"<<endl<<"1-Grabiezca"<<endl<<"69-zakoncz program"<<endl;
+        pomylka:
+        cout<<endl;
+        cin>>x;
+        if(x==0)
+        {
+            system( "cls" );
+            cout<<"Znajdujesz sie w folderze Kruksu, mozesz wybrac program z listy: "<<endl;
+            cout<<"0-Wroc do glownego menu"<<endl;
+            cout<<"1-program1"<<endl;
+            cout<<"2-"<<endl;
+            cout<<"3-"<<endl;
+            cout<<"4-"<<endl;
+            cin>>y;
+            if(y==0) goto menoglowne;
+        }
+        else if (x==1)
+        {
+            system( "cls" );
+            cout<<"Znajdujesz sie w folderze Grabiezca, mozesz wybrac program z listy: "<<endl;
+            cout<<"0-Wroc do glownego menu"<<endl;
+            cout<<"1-program1"<<endl;
+            cout<<"2-program2"<<endl;
+            cout<<"3-"<<endl;
+            cout<<"4-"<<endl;
+            cin>>y;
+            if(y==0) goto menoglowne;
+        }
+        else if (x==69)
+        {
+            system("cls");
+            cout<<"Dziekujemy za wspolprace!";
+        }
+        else
+        {
+            cout<<"Wprowadzona wartosc jest nieprawidlowa";
+            goto pomylka;
+        }
+    }
+};
+
+
+
 int main()
 {
-    int i = 0;
-    cout << "Dzia³a Micha³????????/" << endl;
+    MenuPoczatkowe zmienna;
+    zmienna.funkcja();
 
     return 0;
 }
